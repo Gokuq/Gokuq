9c621f724bd6d820b12000b63869c25118eb5296
gourav kumar <98728332+Gokuq@users.noreply.github.com>
2024-06-03 17:53:32 +0530

    Add files via upload

---
+++ dream_nest-main/g.js
@@ -0,0 +1,29 @@
+function changeImage(fileName){
+    let img = document.querySelector("#bannerImage");
+    img.setAttribute("src", fileName);
+}
+// Get the modal
+var modal = document.getElementById("contactFormModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactUsBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
